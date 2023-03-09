956e531a885a5321ca91fcbf2fd38c5eef94efda
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-09 09:11:19 -0500

    feat(Add Exercise): :sparkles: Exercise 1
    Calculate prom in the array with array.recude((a,b)) => a+b)/array.length

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+/* Construir el algoritmo para un programa que ingrese tres
+notas de un alumno, si el promedio es menor o igual a 3.9
+mostrar un mensaje "Estudie“, de lo contrario un mensaje que
+diga "becado" */
+
 import main from "./components/main.js";
-main.Send();
+main.example();

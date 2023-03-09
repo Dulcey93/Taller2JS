e9499a617851699125e823c93163affcc55fa898
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-09 09:36:18 -0500

    feat(Add Exercise): :sparkles: Exercise 2
    Concat strings with logic operators

--- components/main.js
+++ components/main.js
@@ -1,21 +1,12 @@
 export default {
     example() {
-        // Definimos un array con las tres notas
-        const notas = [];
+        const numero = 15;
 
-        // Pedir al usuario que ingrese las tres notas, con el método push agregamos los datos uno después del otro
-        for (let i = 0; i < 3; i++) {
-            notas.push(parseFloat(prompt(`Ingrese la nota ${i + 1}:`)));
-        }
+        // Comprobar si el número es par/impar y si es mayor que 10
+        const resultado = ["es impar", "es par"][numero % 2] + " y " + (numero > 10 ? "es mayor que 10" : "no es mayor que 10");
 
-        // Calcular el promedio utilizando el método reduce
-        // Con el método reduce declaramos una variable "a" que contenga el acumulativo y "b" como el valor actual del array
-        // el metodo inicia con a=0 y b=notas[0]
-        const promedio = notas.reduce((a, b) => a + b) / notas.length;
+        console.log(`${numero} ${resultado}`);
 
-        // Mostrar el mensaje apropiado utilizando el operador ternario: es promedio menos o igual a 3.9? true: "Estudie" | False: "Becado"
-        const mensaje = promedio <= 3.9 ? "Estudie" : "Becado";
-        console.log(mensaje);
     }
 }
 

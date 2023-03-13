5df42f0e9f1c5de1bb4ad2fb1dc597086cc16b37
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-13 18:38:58 -0500

    feat(Add Exercise): :sparkles: Exercise 9
    Using Getters and Setters

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
-import Figura from './components/Figura.js';
+import Atleta from './components/Atleta.js';
 
-const figuras = [];
-let opcion; // Declarar la variable "opcion" antes del bucle
+const atletas = [];
 
 do {
-  console.log('¿Qué figura deseas ingresar?');
-  console.log('1. Cuadrado');
-  console.log('2. Rectángulo');
-  console.log('3. Salir');
+  console.log('Ingrese el nombre del atleta finalista:');
+  const nombre = prompt('Nombre:');
 
-  opcion = parseInt(prompt('Ingrese una opción:'));
+  console.log(`Ingrese la marca de ${nombre} en metros:`);
+  const marca = parseFloat(prompt('Marca:'));
 
-  switch (opcion) {
-    case 1:
-      const ladoCuadrado = parseFloat(prompt('Ingrese el lado del cuadrado:'));
-      const cuadrado = new Figura(ladoCuadrado, null, null);
-      console.log(`El perímetro del cuadrado es ${cuadrado.calcularPerimetro()}`);
-      figuras.push(cuadrado);
-      break;
+  const atleta = new Atleta(nombre, marca);
+  atletas.push(atleta);
 
-    case 2:
-      const baseRectangulo = parseFloat(prompt('Ingrese la base del rectángulo:'));
-      const alturaRectangulo = parseFloat(prompt('Ingrese la altura del rectángulo:'));
-      const rectangulo = new Figura(null, baseRectangulo, alturaRectangulo);
-      console.log(`El área del rectángulo es ${rectangulo.calcularArea()}`);
-      figuras.push(rectangulo);
-      break;
+  const campeona = atletas.reduce((campeona, atleta) => {
+    return atleta.getMarca() > campeona.getMarca() ? atleta : campeona;
+  });
 
-    case 3:
-      console.log('¡Hasta luego!');
-      break;
+  console.log(`La atleta campeona es ${campeona.getNombre()} con una marca de ${campeona.getMarca()} metros.`);
 
-    default:
-      console.log('Opción inválida.');
-      break;
+  const rompioRecord = campeona.getMarca() > 15.5;
+  console.log(`El récord fue ${rompioRecord ? 'roto' : 'no roto'}.`);
+
+  if (rompioRecord) {
+    console.log('La atleta campeona recibirá un pago de 500 millones.');
   }
 
-} while (opcion !== 3);
-
-const sumaAreas = figuras.reduce((total, figura) => total + figura.calcularArea(), 0);
-console.log(`La suma de las áreas ingresadas es ${sumaAreas}.`);
+  const continuar = prompt('¿Desea ingresar los datos de otra atleta? (S/N)').toUpperCase() === 'S';
+  if (!continuar) {
+    console.log('¡Hasta luego!');
+  }
+} while (continuar);
---
+++ components/Atleta.js
@@ -0,0 +1,14 @@
+export default class Atleta {
+    constructor(nombre, marca) {
+      this.nombre = nombre;
+      this.marca = marca;
+    }
+  
+    getNombre() {
+      return this.nombre;
+    }
+  
+    getMarca() {
+      return this.marca;
+    }
+  }  
